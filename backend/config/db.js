782c9859b7fd889e54bad0fb14929bf909d00446
Paritosh Singh <paritoshsingh1609@gmail.com>
2024-07-12 15:03:50 +0530

    fix: remove depriacated options

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,13 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const connectDB=async ()=>{
+const connectDB = async () => {
    try {
-      const conn=await mongoose.connect(process.env.MONGO_URI,{
-         useNewUrlParser:true,
-         userUnifiedTopology:true,
-      });
+      const conn = await mongoose.connect(process.env.MONGO_URI);
       console.log(`MongoDB Connected: ${conn.connection.host}`);
-
    } catch (error) {
-      console.log(`Error: ${error.message}`);
-      process.exit;
+      console.error(`Error: ${error.message}`);
+      process.exit(1);
    }
-}
+};
 
-module.exports=connectDB;
+module.exports = connectDB;
